354677bdb1990b56371372dfb870954e302976c1
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-06 08:15:48 +0000

    var

--- vars/common.groovy
+++ vars/common.groovy
@@ -17,5 +17,5 @@ def testcases() {
 
 
 def codequality() {
-  sh 'sonar-scanner -Dsonar.host.url=http://172.31.2.143:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+  sh 'sonar-scanner -Dsonar.host.url=http://172.31.2.143:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
 }
