9516630cdf1d208d6bd9e2f6d4948fb2691473d4
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-07 12:42:54 +0000

    all

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -10,10 +10,6 @@ def call() {
       stage('Compile/Build') {
         steps {
           script {
-            withAWSParameterStore(credentialsId: 'PARAM3', naming: 'absolute', path: 'sonarqube.user', recursive: false, regionName: 'us-east-1') {
-             sh 'env'
-              sh 'exit 1'
-            }
             common.compile()
           }
         }
