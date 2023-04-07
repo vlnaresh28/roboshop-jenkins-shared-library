413478c89eee66a8a3866aebaaa34e5785f760b9
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-07 06:26:11 +0000

    USER

--- vars/common.groovy
+++ vars/common.groovy
@@ -18,7 +18,7 @@ def testcases() {
 
 def codequality() {
   withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
-      sh 'sonar-scanner -Dsonar.host.url=http://172.31.2.143:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+      sh 'sonar-scanner -Dsonar.host.url=http://172.31.2.143:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
     }
 
 }
