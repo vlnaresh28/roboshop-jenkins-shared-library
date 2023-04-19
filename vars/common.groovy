011f4c3031a1559964fd36098c525d0b5ccdab33
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-19 04:35:56 +0000

    sonar ip private

--- vars/common.groovy
+++ vars/common.groovy
@@ -20,7 +20,7 @@ def codequality() {
   withAWSParameterStore(credentialsId: 'PARAM', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1')
     {
    
-      sh 'sonar-scanner -Dsonar.host.url=http://172.31.2.143:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts} -Dsonar.qualitygate.wait=true'
+      sh 'sonar-scanner -Dsonar.host.url=http://172.31.1.126:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts} -Dsonar.qualitygate.wait=true'
     
     } 
 }
