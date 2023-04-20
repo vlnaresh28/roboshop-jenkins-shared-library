0f4b6b19cd9e6f233ca6e8c550a6a37af4c1bcfa
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-20 04:12:18 +0000

    f

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -2,17 +2,38 @@ def call() {
   if (!env.sonar_extra_opts) {
     env.sonar_extra_opts=""
   }
-  
+
+  if(env.TAG_NAME ==~ ".*") {
+    env.GTAG = "true"
+  } else {
+    env.GTAG = "false"
+  }
   node('workstation') {
 
     try {
-      stage('Compile/Build') {
-        common.compile()
+
+      stage('Check Out Code') {
+        cleanWs()
+        git branch: 'main', url: 'https://github.com/vlnaresh28/cart'
       }
 
-      stage('Test Cases') {
-        common.testcases()
+      sh 'env'
+
+      if (env.BRANCH_NAME != "main") {
+        stage('Compile/Build') {
+          common.compile()
+        }
       }
+
+      println GTAG
+      println BRANCH_NAME
+
+      if(env.GTAG != "true" && env.BRANCH_NAME != "main") {
+        stage('Test Cases') {
+          common.testcases()
+        }
+      }
+
 
       stage('Code Quality') {
         common.codequality()
